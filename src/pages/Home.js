4689b4ad86ebea64862259a72069dfae2f5301f5
Eino Pitkälä <eiccabot@gmail.com>
2023-05-09 10:12:02 +0300

    Commented out undone change.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import LandingCard from '../components/home/LandingCard'
 import Projects from '../components/home/Projects'
 import React from 'react'
-import MyTech from '../components/home/MyTech'
+// import MyTech from '../components/home/MyTech'
+// MyTech is commented out because it's not finished yet.
 
 export default function Home() {
   return (
     <>
         <LandingCard />
         <Projects />
-        <MyTech />
+        {/* <MyTech /> */}
     </>
   )
 }
